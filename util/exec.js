36dfa333b0ce6e968b807c1761f14c7991578a55
André Cruz <andremiguelcruz@msn.com>
2015-06-23 19:25:06 +0100

    Fix typo and support status codes in exec.

--- util/exec.js
+++ util/exec.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var cp   = require('child_process');
-var fail = require('./fail');
+var cp      = require('child_process');
+var errcode = require('err-code');
+var fail    = require('./fail');
 
 function exec(command, args, options) {
     var ret;
@@ -11,16 +12,17 @@ function exec(command, args, options) {
         args = [];
     }
 
+    options = options || {};
     ret = cp.spawnSync(command, args, options);
 
     if (ret.error) {
-        ret.error.details = 'Command was: ' + command + (' ' + args.join(' ')).trim();
+        ret.error.details = 'Command was: ' + command + ' ' + args.join(' ');
         fail(ret.error);
     }
 
-    if (ret.status) {
-        fail(new Error('Command exited with code ' + ret.status, 'EEXEC', {
-            details: 'Command was: ' + command + (' ' + args.join(' ')).trim()
+    if (ret.status && (!options.status || options.status.indexOf(ret.status) === -1)) {
+        fail(errcode(new Error('Command exited with code ' + ret.status), 'EEXEC', {
+            details: 'Command was: ' + command + ' ' + args.join(' ')
         }));
     }
 
